a6f54dac7ca3bb953b1c6e73976287e38804a9dc
Ronath1 <ronathwijayasiri123@gmail.com>
2025-08-31 12:04:56 +0530

    add details id cards

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -3,10 +3,11 @@ import {View, Text, ScrollView, Image} from 'react-native'
 import {useLocalSearchParams} from "expo-router";
 import useFetch from "@/services/useFetch";
 import {fetchMovieDetails} from "@/services/api";
+import {icons} from "@/constants/icons";
 
 const MovieDetails = () => {
 
-    const{ id } = useLocalSearchParams()
+    const{ id } = useLocalSearchParams();
 
     const {data: movie, loading } = useFetch(() =>
         fetchMovieDetails(id as string));
@@ -16,8 +17,30 @@ const MovieDetails = () => {
         <ScrollView contentContainerStyle={{
             paddingBottom: 80 }}>
             <View>
-                <Image source={{ uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`}}/>
+                <Image source={{ uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`}} className="w-full h-[500px]" resizeMode="stretch"/>
             </View>
+
+            <View className="flex-col items-start justify-center mt-5 px-5">
+                <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+
+            <View className="flex-row items-center gap-x-1 mt-2">
+                <Text className="text-light-200 text-sm">{movie?.release_date?.split('-')[0]}</Text>
+                <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
+
+            </View>
+                <View className="flex-row items-center bg-dark-100 px-2
+                py-1 rounded-md gap-x-1 mt-2">
+                    <Image source={icons.star} className="size-4" />
+                    <Text className="text-white font-bold text-sm">
+                        {Math.round(movie?.vote_average ?? 0)}/10
+                    </Text>
+                    <Text className="text-light-200 text-sm">
+                        ({movie?.vote_count} votes)
+                    </Text>
+
+                </View>
+            </View>
+
         </ScrollView>
        </View>
   )
